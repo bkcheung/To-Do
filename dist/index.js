f9e0311f81db89d29ceb561cb50eef3ee20fcb02
bkcheung <b.cheung@hotmail.com>
2022-03-14 23:19:33 -0400

    New list button creates new project and div on DOM

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 // import './style.css';
 
 const projectFactory = (name) => {
-    return {name}
+    let list = [];
+    return {name, list}
 };
 
 const listItemFactory = (description) => {
@@ -10,3 +11,37 @@ const listItemFactory = (description) => {
     let dueDate = "none";
     return {description, complete, priority, dueDate}
 };
+
+const toDoList = (() => {
+    this.list = [];
+
+    const _makeProjectDiv = (project) => {
+        const container = document.getElementById('container');
+
+        const newDiv = document.createElement('div');
+        newDiv.classList.add("project");
+        newDiv.setAttribute('pName', project.name);
+
+        const newDivHeader = document.createElement('div');
+        newDivHeader.classList.add("projectHeader");
+        newDivHeader.setAttribute('pName', project.name);
+        newDivHeader.innerHTML = project.name;
+
+        const newDivContent = document.createElement('div');
+        newDivContent.classList.add("projectContent");
+        newDivContent.setAttribute('pName', project.name);
+
+        newDiv.append(newDivHeader, newDivContent);
+        container.appendChild(newDiv);
+    }
+
+    const addProject = (name) => {
+        const newProj = projectFactory(name);
+        list.push(newProj);
+        _makeProjectDiv(newProj);
+    }
+
+    return {
+        list, addProject
+    }
+})();
