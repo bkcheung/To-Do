321a07ae7015036a96e4d809920addfd44bd45ac
bkcheung <b.cheung@hotmail.com>
2022-03-15 01:18:20 -0400

    Move DOM manipulation to separate module

--- index.js
+++ index.js
@@ -16,34 +16,51 @@ const listItemFactory = (description) => {
 const toDoList = (() => {
     this.masterList = [];
 
-    const _makeProjectDiv = (project) => {
+    const addProject = (name) => {
+        const newProj = projectFactory(name);
+        newProj.id = masterList.length;
+        masterList.push(newProj);
+        genList.makeProjectDiv(newProj);
+    }
+
+    const addItem = (description, listId) => {
+        const newItem = listItemFactory(description);
+        const project = masterList[listId];
+        project.pList.push(newItem);
+        genList.genPContent(project);
+    }
+
+    return {
+        masterList, addProject, addItem
+    }
+})();
+
+const genList = (() => {
+    const makeProjectDiv = (project) => {
         const container = document.getElementById('container');
 
         const newDiv = document.createElement('div');
         newDiv.classList.add("project");
-        newDiv.setAttribute('pName', project.name);
 
         const newDivHeader = document.createElement('div');
         newDivHeader.classList.add("projectHeader");
-        // newDivHeader.setAttribute('pName', project.name);
         newDivHeader.innerHTML = project.name;
 
         const newDivContent = document.createElement('div');
         newDivContent.classList.add("projectContent");
-        newDivContent.setAttribute('id', project.id); //unique ID for content area
+        newDivContent.setAttribute('id', project.id); //set ID for content area
 
-        // newDivContent.appendChild(newItemButton);
         newDiv.append(newDivHeader, newDivContent);
         container.appendChild(newDiv);
 
-        _genPContent(project);
+        genPContent(project);
 
     }
 
-    const _genPContent = (project) => {
+    const genPContent = (project) => {
         const projectList = project.pList;
         const projectCont = document.getElementById(`${project.id}`);
-        projectCont.innerHTML = '';
+        projectCont.innerHTML = ''; 
 
         projectList.forEach(element => {
             const listItem = document.createElement('li');
@@ -58,24 +75,10 @@ const toDoList = (() => {
         newItemButton.innerText = "Add Item";
         newItemButton.setAttribute('onclick', `toDoList.addItem('New item',${project.id})`);
         projectCont.appendChild(newItemButton);
-        
-    }
 
-    const addProject = (name) => {
-        const newProj = projectFactory(name);
-        newProj.id = masterList.length;
-        masterList.push(newProj);
-        _makeProjectDiv(newProj);
-    }
-
-    const addItem = (description, listId) => {
-        const newItem = listItemFactory(description);
-        const project = masterList[listId];
-        project.pList.push(newItem);
-        _genPContent(project);
     }
 
     return {
-        masterList, addProject, addItem
+        makeProjectDiv, genPContent
     }
 })();
